fe4bd9c7908728d83ad021fb19832eb1bd7760d7
Lilith La Rose <lilithannalise@gmail.com>
2020-04-13 13:16:37 +1000

    Feature - added welcome message

---
+++ src/com/twu/biblioteca/UIStrings.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class UIStrings {
+
+    public static String Welcome() {
+        //message to display on program start
+        return "Welcome to Biblioteca. your one-stop-shop for great book titles in Bangalore!";
+    }
+    public static String Menu(){
+        //main menu options 1. 2. 3. etc
+        return null;
+    }
+}
